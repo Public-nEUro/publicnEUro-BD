b5b9678ba54fe14bcc7aa0c4bd48a7a48484231d
Oscar Rindel Peulicke <oripe@dtu.dk>
2024-08-06 15:51:01 +0200

    fix warning

--- backend/src/config.py
+++ backend/src/config.py
@@ -4,6 +4,7 @@ LOG_LEVEL = "INFO"
 
 SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
 SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 OPENAPI_SERVER_LIST = [
     {"url": "http://localhost/", "description": "Local"},
