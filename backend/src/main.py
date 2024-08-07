2a2d4fe824b874ffabcd09e6069c5b753c24e06b
Oscar Rindel Peulicke <oripe@dtu.dk>
2024-08-07 14:24:27 +0200

    make separate schema file

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,7 +5,8 @@ from apispec_webframeworks.flask import FlaskPlugin
 from flask import Flask, jsonify
 from flask_cors import CORS
 from logging.config import dictConfig
-from .database import db, ma
+from .database import db
+from .schema import ma
 from .errorhandlers import register_errorhandlers
 from .api import init_endpoints as init_api
 from .config import LOG_LEVEL
