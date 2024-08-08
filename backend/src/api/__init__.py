380aa2c386248795c7d4c874839231d80b752890
Oscar Rindel Peulicke <oripe@dtu.dk>
2024-08-08 10:31:12 +0200

    remove unused functions

--- backend/src/api/__init__.py
+++ backend/src/api/__init__.py
@@ -32,69 +32,6 @@ def docstring(description, input_schema, response_description, response_schema):
     return _decorator
 
 
-def docstring_file_upload():
-    description = "Upload a file"
-    doc = f"""
-        {description}
-        ---
-        post:
-            description: {description}
-            requestBody:
-                content:
-                    multipart/form-data:
-                        schema:
-                            type: object
-                            properties:
-                                file:
-                                    type: string
-                                    format: binary
-
-            responses:
-                200:
-                    description: Returns the file id
-                    content:
-                        application/json:
-                            schema:
-                                type: object
-                                properties:
-                                    file_id:
-                                        type: string
-        """
-
-    def _decorator(func):
-        func.__doc__ = doc
-        return func
-
-    return _decorator
-
-
-def docstring_file_download(description, input_schema, response_description):
-    doc = f"""
-        {description}
-        ---
-        post:
-            description: {description}
-            requestBody:
-                content:
-                    application/json:
-                        schema: {input_schema.__name__}
-            responses:
-                200:
-                    description: {response_description}
-                    x-is-file: true
-                    content:
-                        application/octet-stream:
-                            schema:
-                                type: object
-        """
-
-    def _decorator(func):
-        func.__doc__ = doc
-        return func
-
-    return _decorator
-
-
 def change_name(name):
     def _decorator(func):
         func.__name__ = name
@@ -142,22 +79,6 @@ def endpoint(app, function, description="", response_description=""):
     return func
 
 
-def endpoint_file_upload(app, function, auth):
-    @app.post(f"/{function.__name__}")
-    @docstring_file_upload()
-    @change_name(f"api_{function.__name__}")
-    @auth
-    def func():
-        print(type(request.files["file"]), flush=True)
-        input_data = function.__annotations__["request"]().load(
-            {"file": request.files["file"]}
-        )
-        response = function(input_data)
-        return function.__annotations__["return"]().jsonify(response)
-
-    return func
-
-
 def create_response(file, mime_type, file_name):
     return Response(
         file,
@@ -167,21 +88,6 @@ def create_response(file, mime_type, file_name):
             "Access-Control-Expose-Headers": "Content-Disposition",
         },
     )
-
-
-def endpoint_file_download(app, function, description, response_description, auth):
-    @app.post(f"/{function.__name__}")
-    @docstring_file_download(
-        description, function.__annotations__["request"], response_description
-    )
-    @change_name(f"api_{function.__name__}")
-    @auth
-    def func():
-        input_data = function.__annotations__["request"]().load(request.json)
-        response = function(input_data)
-        return create_response(*response)
-
-    return func
 
 
 def init_endpoints(app):
