ab1c12d2082c432360cecc3a9e3dfab4aa246594
Oscar Rindel Peulicke <oripe@dtu.dk>
2024-08-22 12:01:52 +0200

    feat: make deploy script restart

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
 rsync -avz --delete --filter="dir-merge,- .gitignore" --exclude '.git' --exclude '.env' --exclude 'frontend/src/environments/environment.ts' --exclude '.gitignore' --exclude '.deploy.sh' --exclude '.docker-compose.yml' --include '*' . root@10.45.130.67:/dpnru002/shared/group/data-manager
+ssh root@10.45.130.67 'cd /dpnru002/shared/group/data-manager && ./restart-test.sh'
