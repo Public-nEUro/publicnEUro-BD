d22ff28a373031dc047b5eb80f951996f078ec4a
Oscar Rindel Peulicke <oripe@dtu.dk>
2024-08-27 14:26:11 +0200

    clean up

--- backend/src/api/get_users.py
+++ backend/src/api/get_users.py
@@ -3,11 +3,7 @@ from flask_marshmallow import Schema
 from marshmallow import fields
 from ..database.user import User, get_db_approved_users, get_db_non_approved_users
 from .assertions import get_logged_in_admin_or_abort
-from .common_schemas import UserInfo, extract_user_info
-
-
-class GetUsersRequestSchema(Schema):
-    pass
+from .common_schemas import UserInfo, extract_user_info, EmptySchema
 
 
 class GetUsersResponseSchema(Schema):
@@ -18,13 +14,13 @@ def db_users_to_response(users: List[User]):
     return {"users": [extract_user_info(user) for user in users]}
 
 
-def get_approved_users(request: GetUsersRequestSchema) -> GetUsersResponseSchema:
+def get_approved_users(request: EmptySchema) -> GetUsersResponseSchema:
     get_logged_in_admin_or_abort()
 
     return db_users_to_response(get_db_approved_users())
 
 
-def get_non_approved_users(request: GetUsersRequestSchema) -> GetUsersResponseSchema:
+def get_non_approved_users(request: EmptySchema) -> GetUsersResponseSchema:
     get_logged_in_admin_or_abort()
 
     return db_users_to_response(get_db_non_approved_users())
