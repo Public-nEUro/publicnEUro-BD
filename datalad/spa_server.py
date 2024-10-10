c96fa826e9e0c52029fe3f2af2d2e1bb9e832f71
Oscar Rindel Peulicke <oripe@dtu.dk>
2024-10-10 12:19:29 +0200

    feat(datalad): make it work like a single page application so that reloading a page is possible

---
+++ datalad/spa_server.py
@@ -0,0 +1,20 @@
+import os
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+
+class SPARequestHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        first_part = self.path.split("/")[1]
+
+        if not os.path.isdir(first_part):
+            self.path = "/index.html"
+
+        return super().do_GET()
+
+
+os.chdir("/root/repo")
+PORT = 3000
+
+with HTTPServer(("", PORT), SPARequestHandler) as httpd:
+    print(f"Serving files from /root/repo on port {PORT}")
+    httpd.serve_forever()
