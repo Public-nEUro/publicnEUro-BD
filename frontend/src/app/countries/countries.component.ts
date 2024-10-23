6279949a609e20c586c91b8a128b4adc2895962b
Oscar Rindel Peulicke <oripe@dtu.dk>
2024-10-23 11:06:54 +0200

    feat: add countries

--- frontend/src/app/countries/countries.component.ts
+++ frontend/src/app/countries/countries.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { UntypedFormControl, UntypedFormGroup } from "@angular/forms";
 import { Country, DefaultService } from "@services/api-client";
+import { map, Observable, startWith } from "rxjs";
 
 @Component({
     selector: "app-countries",
@@ -9,11 +11,45 @@ import { Country, DefaultService } from "@services/api-client";
 export class CountriesComponent implements OnInit {
     constructor(private service: DefaultService) {}
 
+    addCountryForm: UntypedFormGroup = new UntypedFormGroup({
+        name: new UntypedFormControl(""),
+        geo_location: new UntypedFormControl("")
+    });
+
+    geoLocations: Country.GeoLocationEnum[] = ["EU", "ADEQUATE", "OTHER"];
+    filteredGeoLocations!: Observable<Country.GeoLocationEnum[]>;
+
     countries: Country[] = [];
 
     ngOnInit(): void {
+        this.reload();
+    }
+
+    reload() {
+        this.addCountryForm.reset();
         this.service.apiGetCountriesPost({}).subscribe(res => {
             this.countries = res.countries;
         });
+        this.filteredGeoLocations = this.addCountryForm.get("geo_location")!.valueChanges.pipe(
+            startWith(""),
+            map((value: string) =>
+                this.geoLocations.filter(geoLocation => geoLocation.toLowerCase().includes(value.toLowerCase()))
+            )
+        );
+    }
+
+    get f() {
+        return this.addCountryForm.controls;
+    }
+
+    addCountry() {
+        this.service
+            .apiAddCountryPost({
+                name: this.f["name"].value,
+                geo_location: this.f["geo_location"].value
+            })
+            .subscribe(() => {
+                this.reload();
+            });
     }
 }
