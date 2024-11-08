52f59d3ff5b2018175005fd85cc65cc43a382ac2
Oscar Rindel Peulicke <oripe@dtu.dk>
2024-11-08 13:36:14 +0100

    fix: make sure that deploy scripts are run from root

--- env/prod/deploy.sh
+++ env/prod/deploy.sh
@@ -1,2 +1,7 @@
+[ "$(basename "$PWD")" = "data-manager" ] || {
+    echo "This script should be run from the root of the git-project"
+    exit 1
+}
+
 rsync -avz --delete --filter="dir-merge,- .gitignore" --exclude '.git' --exclude 'env/local' --exclude 'env/test' --exclude 'datalad/docker-mount' --include '*' . root@10.45.130.67:/dpnru002/shared/group/data-manager/prod
 ssh root@10.45.130.67 'cd /dpnru002/shared/group/data-manager/prod/env/prod && ./restart.sh'
--- env/test/deploy.sh
+++ env/test/deploy.sh
@@ -1,2 +1,7 @@
+[ "$(basename "$PWD")" = "data-manager" ] || {
+    echo "This script should be run from the root of the git-project"
+    exit 1
+}
+
 rsync -avz --delete --filter="dir-merge,- .gitignore" --exclude '.git' --exclude 'env/local' --exclude 'env/prod' --exclude 'datalad/docker-mount' --include '*' . root@10.45.130.67:/dpnru002/shared/group/data-manager/test
 ssh root@10.45.130.67 'cd /dpnru002/shared/group/data-manager/test/env/test && ./restart.sh'
