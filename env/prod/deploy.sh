91b6382fc46cfb2cdae63abbe49e95f8a6a857d1
Oscar Rindel Peulicke <oripe@dtu.dk>
2024-11-08 13:30:51 +0100

    fix: exclude docker-mount from rsync

--- env/prod/deploy.sh
+++ env/prod/deploy.sh
@@ -1,2 +1,2 @@
-rsync -avz --delete --filter="dir-merge,- .gitignore" --exclude '.git' --exclude 'env/local' --exclude 'env/test' --include '*' . root@10.45.130.67:/dpnru002/shared/group/data-manager/prod
+rsync -avz --delete --filter="dir-merge,- .gitignore" --exclude '.git' --exclude 'env/local' --exclude 'env/test' --exclude 'datalad/docker-mount' --include '*' . root@10.45.130.67:/dpnru002/shared/group/data-manager/prod
 ssh root@10.45.130.67 'cd /dpnru002/shared/group/data-manager/prod/env/prod && ./restart.sh'
