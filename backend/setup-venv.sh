332295347c3c4377ddb9b46fa51e20a9bc52c030
Oscar Rindel Peulicke <oripe@dtu.dk>
2024-11-12 12:57:08 +0100

    feat: make sure to use python3.10 when setting up venv

--- backend/setup-venv.sh
+++ backend/setup-venv.sh
@@ -1,4 +1,4 @@
-python3 -m venv venv
+python3.10 -m venv venv
 . venv/bin/activate
 pip3 install pip --upgrade
-python3 -m pip install -r ./requirements.txt
+python3.10 -m pip install -r ./requirements.txt
