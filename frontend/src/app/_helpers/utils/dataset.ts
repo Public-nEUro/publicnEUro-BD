d71159b4b457163b49b86b920d6a5a120006fb72
Oscar Rindel Peulicke <oripe@dtu.dk>
2024-11-12 10:14:46 +0100

    feat: show accessibility on request access page

--- frontend/src/app/_helpers/utils/dataset.ts
+++ frontend/src/app/_helpers/utils/dataset.ts
@@ -1,11 +1,13 @@
 export const fieldKeyToLabel: Record<string, string> = {
     name: "Name",
+    accessibility: "Accessibility",
     dua_file_name: "DUA",
     scc_file_name: "SCC"
 };
 
 export const fieldKeyOrder: Record<string, number> = {
     name: 0,
-    dua_file_name: 1,
-    scc_file_name: 2
+    accessibility: 1,
+    dua_file_name: 2,
+    scc_file_name: 3
 };
