fdb6de0c96ee632bd823a2f90a2eeea4aaba680e
Oscar Rindel Peulicke <oripe@dtu.dk>
2024-11-14 14:46:07 +0100

    fix: make separate nginx folder for each environment

--- frontend/src/environments/environment.test.ts
+++ frontend/src/environments/environment.test.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    API_BASE_URL: "http://10.45.130.67:8080",
+    API_BASE_URL: "https://10.45.130.67:8443",
     RECAPTCHA_V3_SITE_KEY: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    API_BASE_URL: "https://localhost",
+    API_BASE_URL: "https://localhost:8443",
     RECAPTCHA_V3_SITE_KEY: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
 };
