af2df53d69cfcc110660064acfa5b214b7bca384
Oscar Rindel Peulicke <oripe@dtu.dk>
2024-11-26 15:14:51 +0100

    feat: remove ability to edit datasets on datasets page

--- frontend/src/app/datasets/datasets.component.ts
+++ frontend/src/app/datasets/datasets.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { downloadBase64, toBase64 } from "@helpers/utils/file";
-import { Dataset, DatasetWithFileData, DefaultService, SccWithId } from "@services/api-client";
+import { downloadBase64 } from "@helpers/utils/file";
+import { Dataset, DefaultService, SccWithId } from "@services/api-client";
 
 @Component({
     selector: "app-datasets",
@@ -10,17 +10,9 @@ import { Dataset, DatasetWithFileData, DefaultService, SccWithId } from "@servic
 export class DatasetsComponent implements OnInit {
     constructor(private service: DefaultService) {}
 
-    accessibilities: Dataset.AccessibilityEnum[] = ["PRIVATE", "EU", "EU_AND_ADEQUATE", "WORLDWIDE", "PUBLIC"];
-    filteredAccessibilities: Dataset.AccessibilityEnum[] = [];
-
     sccs: SccWithId[] = [];
-    filteredSccs: SccWithId[] = [];
-
-    approvalTypes: Dataset.ApprovalTypeEnum[] = ["OVERSIGHT", "AUTOMATED"];
-    filteredApprovalTypes: Dataset.ApprovalTypeEnum[] = [];
 
     datasets: Dataset[] = [];
-    editingDataset: DatasetWithFileData | undefined;
 
     ngOnInit(): void {
         this.reload();
@@ -35,51 +27,12 @@ export class DatasetsComponent implements OnInit {
         });
     }
 
-    edit(dataset: Dataset) {
-        this.editingDataset = dataset as DatasetWithFileData;
-        this.editingDataset.dua_file_data = null;
-        this.filteredAccessibilities = this.accessibilities;
-        this.filteredSccs = this.sccs;
-        this.filteredApprovalTypes = this.approvalTypes;
-    }
-
-    onAccessibilityChange(accessibility: string) {
-        this.filteredAccessibilities = this.accessibilities.filter(a =>
-            a.toLowerCase().includes(accessibility.toLowerCase())
-        );
-    }
-
-    async onDuaFileSelected(event: Event) {
-        if (this.editingDataset === undefined) return;
-        const file = (event.target as HTMLInputElement).files?.[0];
-        if (file === undefined) return;
-        this.editingDataset.dua_file_name = file.name;
-        this.editingDataset.dua_file_data = await toBase64(file);
-    }
-
-    onApprovalChange(approvalType: string) {
-        this.filteredApprovalTypes = this.approvalTypes.filter(a =>
-            a.toLowerCase().includes(approvalType.toLowerCase())
-        );
-    }
-
-    onSccChange(sccTitle: string) {
-        this.filteredSccs = this.sccs.filter(scc => scc.title.toLowerCase().includes(sccTitle.toLowerCase()));
-    }
-
     getScc(sccId: string | null) {
         return this.sccs.find(scc => scc.id === sccId);
     }
 
     getSccTitle(sccId: string) {
         return this.getScc(sccId)?.title ?? "";
-    }
-
-    save(dataset: DatasetWithFileData) {
-        this.editingDataset = undefined;
-        this.service.apiUpdateDatasetPost(dataset).subscribe(() => {
-            this.reload();
-        });
     }
 
     downloadDua(dataset: Dataset) {
