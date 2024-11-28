9dfe486d61326289ccc29b9fcf977be4424cad10
Oscar Rindel Peulicke <oripe@dtu.dk>
2024-11-28 13:13:36 +0100

    fix: exclude tls and ssl from deploy scripts

--- env/prod/deploy.sh
+++ env/prod/deploy.sh
@@ -3,5 +3,5 @@
     exit 1
 }
 
-rsync -avz --delete --filter="dir-merge,- .gitignore" --exclude '.git' --exclude 'env/local' --exclude 'env/test' --exclude 'datalad/docker-mount' --include '*' . root@10.45.130.67:/dpnru002/shared/group/data-manager/prod
+rsync -avz --delete --filter="dir-merge,- .gitignore" --exclude '.git' --exclude 'env/local' --exclude 'env/test' --exclude 'env/prod/nginx/tls' --exclude 'env/prod/nginx/ssl' --exclude 'datalad/docker-mount' --include '*' . root@10.45.130.67:/dpnru002/shared/group/data-manager/prod
 ssh root@10.45.130.67 'cd /dpnru002/shared/group/data-manager/prod/env/prod && ./restart.sh'
--- env/test/deploy.sh
+++ env/test/deploy.sh
@@ -3,5 +3,5 @@
     exit 1
 }
 
-rsync -avz --delete --filter="dir-merge,- .gitignore" --exclude '.git' --exclude 'env/local' --exclude 'env/prod' --exclude 'datalad/docker-mount' --include '*' . root@10.45.130.67:/dpnru002/shared/group/data-manager/test
+rsync -avz --delete --filter="dir-merge,- .gitignore" --exclude '.git' --exclude 'env/local' --exclude 'env/prod' --exclude 'env/test/nginx/tls' --exclude 'env/test/nginx/ssl' --exclude 'datalad/docker-mount' --include '*' . root@10.45.130.67:/dpnru002/shared/group/data-manager/test
 ssh root@10.45.130.67 'cd /dpnru002/shared/group/data-manager/test/env/test && ./restart.sh'
