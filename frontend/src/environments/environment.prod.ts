c24f376229ab54cef31ebc3af22e27d9edd07a5e
Oscar Rindel Peulicke <oripe@dtu.dk>
2025-02-03 11:26:59 +0100

    feat: update prod RECAPTCHA_V3_SITE_KEY

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     API_BASE_URL: "https://datacatalog.publicneuro.eu",
-    RECAPTCHA_V3_SITE_KEY: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+    RECAPTCHA_V3_SITE_KEY: "6LcdV8sqAAAAAIc5W6ALDGuZuG2bHLix795mXBTU"
 };
