bd73f62bfd06d3d00a4d96bf01d75a90df775b9e
Oscar Rindel Peulicke <oripe@dtu.dk>
2025-02-25 12:39:03 +0100

    chore: update test RECAPTCHA_V3_SITE_KEY

--- frontend/src/environments/environment.test.ts
+++ frontend/src/environments/environment.test.ts
@@ -1,4 +1,4 @@
 export const environment = {
     API_BASE_URL: "https://datacatalog.publicneuro.eu:8443",
-    RECAPTCHA_V3_SITE_KEY: "6Lf5lMsqAAAAANos3DBsLo3hSWDyDFhkpQBH_iBn"
+    RECAPTCHA_V3_SITE_KEY: "6LcxJOIqAAAAAGF7MM48lymUq5ER91uluynpF_hH"
 };
