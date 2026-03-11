8b1a16a23dd844b08d61e277b3ade024fb7edab5
Oscar Rindel Peulicke <oripe@dtu.dk>
2026-03-11 14:47:37 +0100

    update deploy scripts

--- env/prod/deploy.sh
+++ env/prod/deploy.sh
@@ -3,5 +3,16 @@
     exit 1
 }
 
-rsync -avz --delete --filter="dir-merge,- .gitignore" --exclude '.git' --exclude 'env/local' --exclude 'env/test' --exclude 'env/prod/nginx/tls' --exclude 'env/prod/nginx/ssl' --exclude 'datalad/docker-mount' --include '*' . root@10.45.130.67:/dpnru002/shared/group/data-manager/prod
-ssh root@10.45.130.67 'cd /dpnru002/shared/group/data-manager/prod/env/prod && ./restart.sh'
+if [ -z "$1" ]; then
+    echo "Please specify your username"
+    exit 1
+fi
+
+USER="$1"
+
+read -s -p "Enter password for $USER: " PASSWORD
+echo
+
+rsync -avz --delete --filter='dir-merge,- .gitignore' --exclude '.git' --exclude 'env/local' --exclude 'env/test' --exclude 'env/prod/nginx/tls' --exclude 'env/prod/nginx/ssl' --exclude 'datalad/docker-mount' --include '*' . $USER@10.45.130.68:/tmp/syncfolder
+ssh $USER@10.45.130.68 "cd /tmp/syncfolder && echo "$PASSWORD" | sudo -S rsync -avz --delete --filter='dir-merge,- .gitignore' --exclude '.git' --exclude 'env/local' --exclude 'env/test' --exclude 'env/prod/nginx/tls' --exclude 'env/prod/nginx/ssl' --exclude 'datalad/docker-mount' --include '*' . /dpnru002/shared/group/data-manager/prod"
+ssh $USER@10.45.130.68 'cd /dpnru002/shared/group/data-manager/prod/env/prod && echo '$PASSWORD' | sudo -S ./restart.sh'
--- env/test/deploy.sh
+++ env/test/deploy.sh
@@ -3,5 +3,16 @@
     exit 1
 }
 
-rsync -avz --delete --filter="dir-merge,- .gitignore" --exclude '.git' --exclude 'env/local' --exclude 'env/prod' --exclude 'env/test/nginx/tls' --exclude 'env/test/nginx/ssl' --exclude 'datalad/docker-mount' --include '*' . root@10.45.130.67:/dpnru002/shared/group/data-manager/test
-ssh root@10.45.130.67 'cd /dpnru002/shared/group/data-manager/test/env/test && ./restart.sh'
+if [ -z "$1" ]; then
+    echo "Please specify your username"
+    exit 1
+fi
+
+USER="$1"
+
+read -s -p "Enter password for $USER: " PASSWORD
+echo
+
+rsync -avz --delete --filter='dir-merge,- .gitignore' --exclude '.git' --exclude 'env/local' --exclude 'env/test' --exclude 'env/test/nginx/tls' --exclude 'env/test/nginx/ssl' --exclude 'datalad/docker-mount' --include '*' . $USER@10.45.130.68:/tmp/syncfolder
+ssh $USER@10.45.130.68 "cd /tmp/syncfolder && echo "$PASSWORD" | sudo -S rsync -avz --delete --filter='dir-merge,- .gitignore' --exclude '.git' --exclude 'env/local' --exclude 'env/test' --exclude 'env/test/nginx/tls' --exclude 'env/test/nginx/ssl' --exclude 'datalad/docker-mount' --include '*' . /dpnru002/shared/group/data-manager/test"
+ssh $USER@10.45.130.68 'cd /dpnru002/shared/group/data-manager/test/env/test && echo '$PASSWORD' | sudo -S ./restart.sh'
