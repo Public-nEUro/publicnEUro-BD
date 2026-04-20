fad6a4214309ca6a3ef888e44ab4f2c94511dded
Oscar Rindel Peulicke <oripe@dtu.dk>
2026-04-20 13:21:57 +0200

    fix: force update the repo

--- datalad/update_repo.sh
+++ datalad/update_repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -d "repo" ] && [ -d "repo/.git" ]; then
-    cd repo && git pull
+    cd repo && git fetch origin && git reset --hard origin/master
 else
     git clone https://github.com/Public-nEUro/DataCatalogue.git repo
 fi
