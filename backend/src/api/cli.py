20976ca40064272d27df0c045585febefcce4f36
Oscar Rindel Peulicke <oripe@dtu.dk>
2026-07-27 14:22:03 +0200

    feat: make cli talk to our own backend

--- backend/src/api/cli.py
+++ backend/src/api/cli.py
@@ -5,7 +5,6 @@ def get_cli_script(insecure: bool) -> str:
     insecure_str = " --insecure" if insecure else ""
     no_check_certificate_str = " --no-check-certificate" if insecure else ""
     frontend_url = os.environ["FRONTEND_URL"]
-    delphi_backend_url = os.environ["DELPHI_BACKEND_URL"]
 
     return f"""#!/bin/bash
 
@@ -15,7 +14,7 @@ read -p "Enter the dataset ID: " dataset
 read -p "Enter the folder path to download: " folder_path
 
 get_share_link_url="{frontend_url}/api/get_share_link"
-prepare_url="{delphi_backend_url}/project_management/file_management/download/prepare"
+prepare_url="{frontend_url}/api/prepare_zip"
 
 share_link=$(curl{insecure_str} -w "\\n" -u "$email":"$password" -L "$get_share_link_url"/"$dataset")
 
@@ -28,7 +27,7 @@ fi
 share_link_unescaped=$(printf '%b' "${{share_link//%/\\\\x}}")
 share_auth=$(basename "$share_link_unescaped")
 
-prepare_response=$(curl{insecure_str} --location "$prepare_url" --header 'Content-Type: application/json' --data "{{\\"share_auth\\": \\""$share_auth"\\",\\"paths\\": [\\""$folder_path"\\"],\\"authorization_header\\": null,\\"project_id\\": null}}")
+prepare_response=$(curl{insecure_str} --location "$prepare_url" --header 'Content-Type: application/json' --data "{{\\"share_auth\\": \\""$share_auth"\\",\\"paths\\": [\\""$folder_path"\\"]}}")
 
 download_name=$(echo $prepare_response | grep -o '"file_name":"[^"]*' | cut -d'"' -f4)
 download_link=$(echo $prepare_response | grep -o '"url":"[^"]*' | cut -d'"' -f4)
